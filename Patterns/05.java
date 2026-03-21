7469dc2868a0ec49b21fe7f1abc6b375fe5fbdb2
Shristyhehe <163558468+Shristyhehe@users.noreply.github.com>
2026-03-21 10:46:23 +0530

    Create 05.java

---
+++ Patterns/05.java
@@ -0,0 +1,23 @@
+class Main {
+    public static void main(String[] args) {
+        int n = 7;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(i==1 || i==n || j==1 || j==n){
+                    System.out.print("4");
+                }
+                else if(i==2 || i==(n-1) || j==2 || j==(n-1)){
+                    System.out.print("3");
+                }
+                else if(i==3 || i==(n-2) || j==3 || j==(n-2)){
+                    System.out.print("2");
+                }
+                else{
+                    System.out.print("1");
+                }
+            }
+             System.out.println("");
+        }
+       
+    }
+}
