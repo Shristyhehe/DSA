58e970bd9725a58b7d73ed3cffb5d524f16cbce4
Shristyhehe <163558468+Shristyhehe@users.noreply.github.com>
2026-03-29 12:41:38 +0530

    Create 04.java

---
+++ Array/04.java
@@ -0,0 +1,25 @@
+//Take an array of numbers as input and check if it is an array sortedd in acending order 
+import java.util.Scanner;
+class Arrays{
+    public static void main(String args[]){
+     System.out.println("Enter the size of the array");
+     Scanner sc = new Scanner(System.in);
+     int size = sc.nextInt(); // size of user
+     int arr[] = new int[size];
+     System.out.println("Enter the no. of  elements in array");
+     for(int i=0; i<size; i++){ // taking input from user
+      arr[i] = sc.nextInt();
+     }
+     int prev = arr[0]; // we have declare here because upar declare isliye nhi kiya kyuki  upar array he intialize nhi hua tha
+     int curr;
+     for(int i=1; i<arr.length; i++){
+         curr = arr[i];
+         if(prev > curr){
+             System.out.println("Array is not sorted");
+             return;
+         }
+         prev = curr; // aur curr toh chal he according to arr[i];
+     }
+     System.out.println("Array is sorted");
+    }
+}
