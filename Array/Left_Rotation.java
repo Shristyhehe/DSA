49a4d5f47e98c28b87109ff757ce9025e1faf561
Shristyhehe <chhetrishristy93@gmail.com>
2026-05-24 15:39:18 +0530

    Update Left_Rotation.java

--- Array/Left_Rotation.java
+++ Array/Left_Rotation.java
@@ -1,4 +1,5 @@
 // time complexity -> O(n)
+// brute force appraoch
 import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
@@ -21,6 +22,37 @@ class Main {
         /*here i is accessing from array of index 1 but in curr we need to insert from 0th index ...and the last element of curr will be first element of array(0th index)*/
         for(int i=0; i<curr.length; i++){
              System.out.print(curr[i] + " ");
+        }
+       
+    }
+}
+
+
+// OPTIMAL APPROACH
+//time complexity 0(n)
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of an array :");
+        int size = sc.nextInt();
+        System.out.println("enter the elements of an array :");
+        int arr[] = new int[size];
+        for(int i=0; i<size; i++){
+            arr[i] = sc.nextInt();
+        }
+        int temp = arr[0];
+         
+        for(int i=0 ; i<arr.length-1; i++){
+            arr[i] = arr[i+1];
+            // bringing the elements one place aage khud k index k
+            
+        }
+        arr[size - 1] = temp; 
+        //jo index 0 ka elemnt hai woh aayega last index pe
+        System.out.println("sorted array after the left once");
+        for(int i=0; i<arr.length; i++){
+             System.out.print(arr[i] +" ");
         }
        
     }
