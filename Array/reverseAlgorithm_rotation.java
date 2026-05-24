fb0148e6f6a8fab636bd49867d79e00e882e45db
Shristyhehe <chhetrishristy93@gmail.com>
2026-05-24 20:03:27 +0530

    Create reverseAlgorithm_rotation.java

---
+++ Array/reverseAlgorithm_rotation.java
@@ -0,0 +1,40 @@
+// REVERSAL ALGORITHM(rotation)
+//TIME COMPLEXITY 0(N) AS SAME AS BRUTE FORCE BUT SC IS 0(1)
+// to understand this code page no 39 of notebook
+
+
+import java.util.Scanner;
+class Main{
+  public static void reverse(int arr[], int start, int end){
+      while(start < end){
+          int temp = arr[start];
+          arr[start] = arr[end];
+          arr[end] = temp;
+          start++;
+          end--;
+      }
+  }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of the array :");
+        int size = sc.nextInt();
+        System.out.println("enter the no of rotations K :");
+        int k = sc.nextInt();
+        System.out.println("enter the elements in the array :");
+        int arr[] = new int[size];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        k = k % size;
+        // reverse the first k elements
+        reverse(arr,0,k-1);
+        //reverse the remaining elemnts
+        reverse(arr, k, size-1);
+        //reversing the whole array
+        reverse(arr,0, size-1);
+        //printing the arrays
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
