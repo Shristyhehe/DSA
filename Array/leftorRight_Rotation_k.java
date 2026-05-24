bc290810f3ce6c9d522cbf847e40d9b419e10968
Shristyhehe <chhetrishristy93@gmail.com>
2026-05-24 18:14:20 +0530

    Create leftorRight_Rotation_k.java

---
+++ Array/leftorRight_Rotation_k.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        int k = sc.nextInt();
+    //for left rotation   
+        for(int i=0; i<size; i++){
+            arr[i] = sc.nextInt();
+        }
+         
+            for(int j=0 ; j<k; j++){
+                int temp = arr[0];
+                //for left rotation
+                for(int i=0; i<arr.length-1; i++){
+                     arr[i] = arr[i+1];
+                    
+                }
+                 arr[arr.length-1]= temp;
+            }
+           
+        for(int i=0; i<arr.length; i++){
+             System.out.println(arr[i] + " ");
+        }
+       
+       
+       
+       
+       //for right  rotation
+     for(int j=0; j<k; j++){
+         int temp = arr[arr.length-1]
+         for(int i=0; i<arr.length-1; i++){
+             arr[i+1] = arr[i]
+     }
+     arr[0] = temp;
+      }
+    }
+}
