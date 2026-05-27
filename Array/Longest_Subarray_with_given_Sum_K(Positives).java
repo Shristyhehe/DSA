b9c2bb2bca2317c5ac583f0eb860f8ee5addfb2a
Shristyhehe <chhetrishristy93@gmail.com>
2026-05-27 10:33:28 +0530

    (for_positives and zero) Longest_Subarray_with_given_Sum_K(Positives).java

--- Array/Longest_Subarray_with_given_Sum_K(Positives).java
+++ Array/Longest_Subarray_with_given_Sum_K(Positives).java
@@ -29,3 +29,55 @@ class Main {
       System.out.println("Longest subarray length with sum " + k + " is : " + max);   
 }
 }
+
+
+//Slinding window(for positives and zero)
+/*tc ->O(2N) because:
+left pointer moves at most N times
+right pointer moves at most N times
+So total operations:
+N + N = 2N
+But in Big-O notation:
+constants are ignored.
+So:
+O(2N)=O(N)*/
+
+
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the size of the array : ");
+        int n = sc.nextInt(); // for size
+        System.out.print("enter the target sum K : ");
+        int k = sc.nextInt();
+        System.out.print("enter the elements of the array :");
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] =sc.nextInt();
+        }
+      int maxLength =0;
+      int right = 0;
+      int left =0;
+      int sum =arr[0];
+      while(right < n){
+          while (left <= right &&  sum > k){
+              sum = sum - arr[left];
+              left++;
+          }
+          if(sum == k){
+              int len = right - left + 1;
+              
+          if(len > maxLength){
+              maxLength = len;
+          }
+      }
+      right ++;
+      if(right < n){
+          sum = sum + arr[right];
+      }
+}
+
+    System.out.println("Longest subarray length is: " +maxLength);
+}
+}
