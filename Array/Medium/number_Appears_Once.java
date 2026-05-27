036f4dbe6376b99e5ecaa882e6ddefcac5a81851
Shristyhehe <chhetrishristy93@gmail.com>
2026-05-27 13:42:16 +0530

    Finding_number_Appears_Once.java

--- Array/Medium/number_Appears_Once.java
+++ Array/Medium/number_Appears_Once.java
@@ -13,7 +13,7 @@ class Solution{
     }
     for(int i=0; i<arr.length; i++){
       int count =0;
-      for(int i=0; i<arr.length; i++){
+      for(int j=0; j<arr.length; j++){
         if(arr[i] == arr[j]){
           count++;
         }
