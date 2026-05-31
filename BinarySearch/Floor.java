1b9441409eb3721e2167e760f7c1d576606d6668
Shristyhehe <chhetrishristy93@gmail.com>
2026-05-31 15:33:40 +0530

    Update Floor.java

--- BinarySearch/Floor.java
+++ BinarySearch/Floor.java
@@ -9,21 +9,19 @@ class Main {
     int ans = -1; // here we have taken -1 not n because here if element is not found return -1;
 while(low <= high){
     int mid = low + (high - low)/2;
-    //element chota hai target se toh hum right side jayenge to check 
-          //aur bada elment chota then target
-          //whatever you get here will also be largest than previous one because search eliminates the search space
+   /* if elelemt is smaller than the target we will move to right side to check whether largest element smaller than or equal to target exists*/
+    
     if(arr[mid] <= target){
-        // here we are storing the element if question is index then ans = mid;
-            ans = arr[mid];
+       
+            ans = arr[mid];   // here we are storing the element if question is index then ans = mid; 
         low = mid + 1;
     }
-    // if element he bada aagya targt se
+    
     else{
-        high = mid - 1;
+        high = mid - 1;  // if element he bada aagya targt se move to left side
     }
 }
-//return the largest index the element which is smaller than target
-return ans;
+return ans;  //return the largest index the element which is smaller than target
 }
     
     public static void main(String[] args) {
