6eef96a2fc592f4180eda1241e585ed51a92f7f8
Shristyhehe <chhetrishristy93@gmail.com>
2026-06-04 15:01:13 +0530

    using hash with array

---
+++ BasicHashing/character_Counting.java
@@ -0,0 +1,23 @@
+//character counting using hash with array
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the string : ");
+        ///for small letter and similarly for capital letter
+        // but for 256 char no need to write int[26]
+       String s = sc.next(); 
+       int hash[] = new int[26];
+        for(int i=0; i<s.length();i++){
+           hash[s.charAt(i) - 'a']++;
+        }
+        // because in hashing using array we need the max no to create the hash
+        for(int i=0; i<hash.length; i++){
+            // the element which is greater than 0 return
+            if(hash[i] > 0){
+               System.out.println((char)(i+'a') +" " +hash[i]); 
+            }
+        }
+        
+    }
+}
