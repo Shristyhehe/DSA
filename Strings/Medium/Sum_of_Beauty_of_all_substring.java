a7a5d86ba8ec4b6dcfef718c86561913ca838b98
Shristyhehe <chhetrishristy93@gmail.com>
2026-06-11 20:19:30 +0530

    beauty = max freq - min freq

--- Strings/Medium/Sum_of_Beauty_of_all_substring.java
+++ Strings/Medium/Sum_of_Beauty_of_all_substring.java
@@ -59,10 +59,11 @@ public int beautySum(String s) {
   int sum = 0;
 for(int i=0; i<s.length; i++){
   int[] freq = new int[26];
-   int max = 0;
-int min = Integer.MAX_VALUE;
+
  for(int j=i; j<s.length; j++){
    freq[s.charAt(j) - 'a']++;
+      int max = 0;
+int min = Integer.MAX_VALUE;
    for(int k=0; k<26; k++){
      if(freq[k] > 0) {
        max = Math.max(max, freq[k]);
