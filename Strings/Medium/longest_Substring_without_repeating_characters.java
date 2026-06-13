3977ed19d1964a5287f7eb495d82de05fbba1029
Shristyhehe <chhetrishristy93@gmail.com>
2026-06-13 11:09:29 +0530

    using sliding window

--- Strings/Medium/longest_Substring_without_repeating_characters.java
+++ Strings/Medium/longest_Substring_without_repeating_characters.java
@@ -1,3 +1,60 @@
+// using sliding window
+first i thought that i need t shrink the window until duplicate ch is found but the key idea was if duplicate exists then we need to remove it from the set by moving leftt
+    until the ch doesn't exist in the set  
+
+class Solution {
+    public int longestUniqueSubstr(String s) {
+        // code here
+        if( s == null){
+            return 0;
+        }
+        int left = 0;
+        int result = 0;
+        HashSet<Character> set = new HashSet<>();
+        for(int right = 0; right < s.length(); right++){
+            char ch = s.charAt(right);
+            if(!set.contains(ch)){
+                set.add(ch);
+            }
+            else{ 
+                // if ch exists then until and unless the ch is not there in the set
+                while(set.contains(ch)){
+                    set.remove(s.charAt(left));
+                    left++;
+                }
+                set.add(ch); // if ch is not there then add the ch
+            }
+            if(result < right - left + 1){
+                result = right - left + 1;
+            }
+        }
+        return result;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // this is optimal but more optimal does exists
 class Solution {
     public int lengthOfLongestSubstring(String s) {
