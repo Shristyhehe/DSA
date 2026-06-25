28f26a5e368ead5f3afa392b2ed428675071c8a9
Shristyhehe <chhetrishristy93@gmail.com>
2026-06-25 11:20:44 +0530

    Boyer-Moore Voting Algorithm

--- Array/Medium/majority_ELement.java
+++ Array/Medium/majority_ELement.java
@@ -1,6 +1,13 @@
 /*if in question says element exists then use one pass Boyer-Moore Voting Algorithm (optimal)
     if it doesn't says that element exists or says may or may not exists then use two pass boyer moore voting tc ans sc is O(n) andO(1)
-     Boyer-Moore Voting Algorithm finds the majority element by repeatedly canceling out different elements. The majority element appears more than n/2 times, so it cannot be completely canceled and survives as the final candidate.
+     Boyer-Moore Voting Algorithm finds the majority element by repeatedly canceling out different elements. 
+     The majority element appears more than n/2 times, so it cannot be completely canceled and survives as the final candidate.
+     The main idea of Boyer–Moore is:
+
+Different elements cancel each other out.
+
+If an element appears more than n/2 times, then even after all possible cancellations with other elements, it will still survive.
+     
      That's the whole story. candidate = current winner, count = vote balance. when element exists ->*/
 class Solution {
     public int majorityElement(int[] nums) {
