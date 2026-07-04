965986e98c3e4a05f3aa5570d20c65a631995687
Shristyhehe <chhetrishristy93@gmail.com>
2026-07-04 10:01:47 +0530

    without stringbuilder or any other string

--- Strings/Easy/valid_Palindorme(without sb).java
+++ Strings/Easy/valid_Palindorme(without sb).java
@@ -13,7 +13,7 @@ class Solution {
           // converting the start ch into lowercase similarly for end
             char ch = Character.toLowerCase(s.charAt(start));
             char sh = Character.toLowerCase(s.charAt(end));
-        }
+        
           //if the ch is not alphabteic then start++ and ch will contain small letter and start < end
             while(!Character.isLetterOrDigit(ch) && start < end){
                 start++;
@@ -37,3 +37,23 @@ class Solution {
         return true;
     }
 }
+
+
+
+
+
+
+
+
+
+start never goes back
+end never goes back
+
+So the inner loops are not doing N work for every outer iteration. They are sharing the same total N movements.
+
+A useful way to think about it:
+
+Each character is examined at most once from the left
+and at most once from the right.
+
+So even though you see three while loops, the total number of pointer movements is still proportional to the length of the string.
