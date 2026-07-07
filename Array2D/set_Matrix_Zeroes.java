683228d79fcc99e71f23db37dce4e41bf3186130
Shristyhehe <chhetrishristy93@gmail.com>
2026-07-07 17:48:17 +0530

    uses two hashset to store rows and columns with zero

--- Array2D/set_Matrix_Zeroes.java
+++ Array2D/set_Matrix_Zeroes.java
@@ -1,21 +1,36 @@
 HASHSET SOLUTION (BETTER APROACH) 
  
+import java.util.HashSet;
+
 class Solution {
     public void setZeroes(int[][] matrix) {
+
+        // Stores rows containing a zero
         HashSet<Integer> rows = new HashSet<>();
-        HashSet<Integer> columns = new HashSet<>();
-        for(int i=0;  i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                   if(matrix[i][j]==0){
-                       rows.add(i);
-                       columns.add(j);
-                       
-                   }
+
+        // Stores columns containing a zero
+        HashSet<Integer> cols = new HashSet<>();
+
+        // Pass 1: Find all zeros and store their row/column
+        for (int i = 0; i < matrix.length; i++) {
+
+            for (int j = 0; j < matrix[0].length; j++) {
+
+                if (matrix[i][j] == 0) {
+
+                    rows.add(i);
+                    cols.add(j);
+                }
             }
         }
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                if(rows.contains(i) || columns.contains(j)){
+
+        // Pass 2: Make cell zero if its row or column is marked
+        for (int i = 0; i < matrix.length; i++) {
+
+            for (int j = 0; j < matrix[0].length; j++) {
+
+                if (rows.contains(i) || cols.contains(j)) {
+
                     matrix[i][j] = 0;
                 }
             }
